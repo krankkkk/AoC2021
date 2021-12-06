30b51a04668b83175cde27b485fcc612d0346e7a
krankkk <home.tim_krause@hotmail.de>
2021-12-06 12:06:49 +0100

    Day6

--- src/main/java/de/aoc/days/day6/Day6.java
+++ src/main/java/de/aoc/days/day6/Day6.java
@@ -3,51 +3,52 @@ package de.aoc.days.day6;
 import de.aoc.days.AbstractDay;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Day6 extends AbstractDay {
+
+    public static final int STATE_COUNT = 9;
+
     @Override
     public long part1(Stream<String> stream) {
         return iterate(stream, 80);
     }
 
     private long iterate(Stream<String> stream, final int iterateCount) {
-        Map<Integer, Long> fish = stream
-                .map(line -> line.split(","))
+        long[] fishPerDay = getArray(stream);
+
+        for (int i = 0; i < iterateCount; i++) {
+            final long[] newFish = new long[STATE_COUNT];
+            for (int j = 0; j < fishPerDay.length; j++) {
+                if (j == 0) {
+                    newFish[6] = newFish[6] + fishPerDay[0];
+                    newFish[8] = fishPerDay[0];
+                } else {
+                    newFish[j - 1] = newFish[j - 1] + fishPerDay[j];
+                }
+            }
+
+            fishPerDay = newFish;
+        }
+
+        return LongStream.of(fishPerDay).sum();
+    }
+
+    private long[] getArray(Stream<String> stream) {
+        Map<Integer, Long> map = stream.map(line -> line.split(","))
                 .flatMap(Arrays::stream)
                 .filter(Predicate.not(String::isBlank))
                 .map(Integer::parseInt)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        fillNullFields(fish);
 
-        for (int i = 0; i < iterateCount; i++) {
-            Map<Integer, Long> newFish = fillNullFields(new HashMap<>());
-            fish.forEach((key, value) -> {
-                if (key == 0) {
-                    newFish.put(6, newFish.get(6) + value);
-                    newFish.put(8, value);
-                } else {
-                    newFish.put(key - 1, newFish.get(key - 1) + value);
-                }
-            });
-            fish = newFish;
-        }
-
-        return fish.values()
-                .stream()
-                .mapToLong(Long::longValue)
-                .sum();
-    }
-
-    private Map<Integer, Long> fillNullFields(final Map<Integer, Long> fish) {
-        IntStream.rangeClosed(0, 8).forEach(c -> fish.putIfAbsent(c, 0L));
-        return fish;
+        final long[] fishPerDay = new long[STATE_COUNT];
+        map.forEach((k, v) -> fishPerDay[k] = v);
+        return fishPerDay;
     }
 
     @Override
